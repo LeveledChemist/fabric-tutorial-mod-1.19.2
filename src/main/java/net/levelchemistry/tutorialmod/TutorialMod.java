f76eb12c311b2a7ba6eb245f142c035b702164db
LevelChem <88857473+LeveledChemist@users.noreply.github.com>
2022-10-30 09:45:38 -0500

    added raw_Tanzanite and Tanzanite to the game, no functionality atm

--- src/main/java/net/levelchemistry/tutorialmod/TutorialMod.java
+++ src/main/java/net/levelchemistry/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.levelchemistry.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.levelchemistry.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 	}
 }
