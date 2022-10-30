9f7e50245d49f00d4563e364b80443b2603fc7ab
LeveledChemist <ethanleepowell@gmail.com>
2022-10-30 08:48:22 -0500

    added comment to line 7 on TutorialMod class

---
+++ src/main/java/net/levelchemistry/tutorialmod/TutorialMod.java
@@ -0,0 +1,16 @@
+package net.levelchemistry.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//This do be a tutorial
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/levelchemistry/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.levelchemistry.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
