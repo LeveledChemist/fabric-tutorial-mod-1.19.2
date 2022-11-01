85a9a34432fb97eb97029184eec1c5b2709eb527
LevelChem <88857473+leveledchemist@users.noreply.github.com>
2022-11-01 07:58:00 -0500

    Added Item Tab to item stack menu in creative

--- src/main/java/net/levelchemistry/tutorialmod/item/ModItems.java
+++ src/main/java/net/levelchemistry/tutorialmod/item/ModItems.java
@@ -9,10 +9,10 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
     public static final Item TANZANITE = registerItem("tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
 
     private static Item registerItem(String name, Item item) {
